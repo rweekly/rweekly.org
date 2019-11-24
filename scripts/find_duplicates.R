ff24648128326ebdc230ab45ced88ddd465f41fb
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-11-24 13:35:32 +0100

    dup script (#589)
    * dup script
    
    * no more events
    
    * done
    
    * Update scripts/find_duplicates.R
    
    Co-Authored-By: Jonathan Carroll <jono@jcarroll.com.au>
    
    * Update scripts/find_duplicates.R
    
    Co-Authored-By: Jonathan Carroll <jono@jcarroll.com.au>
    
    * Update scripts/find_duplicates.R
    
    Co-Authored-By: Jonathan Carroll <jono@jcarroll.com.au>
    
    * tweaks

---
+++ scripts/find_duplicates.R
@@ -0,0 +1,46 @@
+library("magrittr")
+
+acceptable_dups <- function() {
+  c("http://developer.r-project.org/blosxom.cgi/R-devel/NEWS")
+}
+
+
+get_links <- function(path){
+  path %>%
+    readLines(warn = FALSE) %>%
+    .[1:which(grepl("Upcoming Events", .))] %>%
+    commonmark::markdown_xml() %>%
+    xml2::read_xml() %>%
+    xml2::xml_ns_strip() %>%
+    xml2::xml_find_all(xpath = './/link') %>%
+    xml2::xml_attr("destination")
+}
+
+get_dups <- function(rweekly_path = getwd()){
+  old_posts <- fs::dir_ls(file.path(
+    rweekly_path,"_posts"))
+  
+  old_posts <- old_posts[(length(old_posts) - 20):length(old_posts)]
+  
+  
+  old_links <- unlist(purrr::map(old_posts, get_links))
+  
+  thisweek <- get_links(file.path(rweekly_path,
+                                  "draft.md"))
+  current_dups <- thisweek[duplicated(thisweek)]
+  if (length(current_dups)) {
+    message("Duplicates found in current week:")
+    cat(paste0(current_dups, collapse = "\n"))
+    cat("\n")
+  }
+  
+  dups <- thisweek[thisweek %in% old_links]
+  dups <- setdiff(dups, acceptable_dups())
+
+  if (length(dups) == 0) {
+    message("No duplicate links, well done! :-)")
+  } else {
+    return(dups)
+  }
+  
+}
