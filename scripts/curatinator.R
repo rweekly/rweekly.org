a7b7304d72365fb9195d9b275533c89c61c7bde2
Jonathan Carroll <github@jcarroll.com.au>
2023-10-21 12:45:41 +1030

    add newlines in curatinator output

--- scripts/curatinator.R
+++ scripts/curatinator.R
@@ -3,5 +3,5 @@ f <- read.csv("rss_feeds.csv")
 f <- f[f$ENABLE == 1, , drop = FALSE]
 x <- get_rss_posts(f$URL)
 
-cat(x, file = "curatinator_latest.md")
+cat(x, file = "curatinator_latest.md", sep = "/n")
 
