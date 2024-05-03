bf5313cd341e427204322e1e44850577cb77c7a5
colin <colin@thinkr.fr>
2024-05-03 20:47:18 +0200

    feat: update draft

--- scripts/curatinator.R
+++ scripts/curatinator.R
@@ -5,9 +5,9 @@ OUTPUT_FILE <- "curatinator_latest.md"
 source("scripts/get_rss.R")
 f <- read.csv("rss_feeds.csv")
 f <- f[f$ENABLE == 1, , drop = FALSE]
-x <- get_rss_posts(f$URL)
+x <- get_rss_posts(f$URL, since_days_ago = 6)
 
-cat("# RSS POSTS: ##\n\n", file = OUTPUT_FILE) # start of overwriting 
+cat("# RSS POSTS: ##\n\n", file = OUTPUT_FILE) # start of overwriting
 
 cat(x, file = OUTPUT_FILE, sep = "\n", append = TRUE)
 
@@ -47,7 +47,7 @@ process_cranberries <- function(feed_type, start_date, end_date = as.Date(lubrid
   # form the URL based on type (either "new" or "updated")
   cb_url <- glue::glue("https://dirk.eddelbuettel.com/cranberries/cran/{feed_type}/index.rss")
   ftype <- feed_type
-  
+
   # process the new packages feed
   cb_tidy <- tidyRSS::tidyfeed(cb_url) %>%
     mutate(feed_type = ftype) %>%
@@ -55,23 +55,23 @@ process_cranberries <- function(feed_type, start_date, end_date = as.Date(lubrid
     select(item_title, item_link, item_description, item_pub_date, feed_type) %>%
     distinct() %>%
     mutate(package_name = purrr::map_chr(item_link, ~tidy_package_name(.x))) %>%
-    
+
     # leverage the awesome pkgsearch package to get metadata
     mutate(package_meta = purrr::map(package_name, ~pkgsearch::cran_package(.x)),
            package_version = purrr::map_chr(package_meta, "Version"),
            package_title = purrr::map_chr(package_meta, "Title"),
            package_date = purrr::map_chr(package_meta, "Date/Publication")) %>%
-    
+
     filter(item_pub_date >= as.Date(start_date) & item_pub_date <= as.Date(now())) %>%
     select(one_of(c("package_version", "package_name", "package_title", "feed_type",  "package_date"))) %>%
     mutate(markdown_string = purrr::pmap_chr(select(., -package_date), create_text, add_update_titles = TRUE))
-  
+
   return(cb_tidy)
 }
 
 
 # obtain updated packages
-cb_updated_df <- process_cranberries(feed_type = "updated", start_date = as.Date(Sys.Date() - 10))
+cb_updated_df <- process_cranberries(feed_type = "updated", start_date = as.Date(Sys.Date() - 6))
 
 cat("\n# CRANberries UPDATED: ##\n", file = OUTPUT_FILE, sep = "\n", append = TRUE)
 
@@ -79,7 +79,7 @@ cat("\n# CRANberries UPDATED: ##\n", file = OUTPUT_FILE, sep = "\n", append = TR
 cat(cb_updated_df$markdown_string, file = OUTPUT_FILE, sep = "\n", append = TRUE)
 
 # obtain new packages
-cb_new_df <- process_cranberries(feed_type = "new", start_date = as.Date(Sys.Date() - 10))
+cb_new_df <- process_cranberries(feed_type = "new", start_date = as.Date(Sys.Date() - 6))
 
 cat("\n\n# CRANberries NEW: ##\n\n", file = OUTPUT_FILE, sep = "\n", append = TRUE)
 
@@ -93,4 +93,5 @@ cat("\n\n", file = OUTPUT_FILE, append = TRUE)
 collected <- readLines(file(OUTPUT_FILE))
 
 cat(unique(collected), file = OUTPUT_FILE, sep = "\n") # overwrite with de-dup
+
 
